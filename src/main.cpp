663d74f10a1b95bca60ad9387a93648bf869378a
John P. Swensen <jpswensen@gmail.com>
2023-04-28 12:41:35 -0700

    Commit to pare down a little more for a minimal test

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,30 @@
 #include <Arduino.h>
-#include <i2c_driver.h>
-#include "imx_rt1060/imx_rt1060_i2c_driver.h"
 
 #include "common.h"
 #include "port_io.h"
 
-// Variables for storing the mode of operation and any error codes
+// // Variables for storing the mode of operation and any error codes
 volatile uint8_t lastTriggerValue = 0;
 volatile Mode_t mode = INITIALIZING;
-volatile Error_t errCode = NONE_ERROR;
+// volatile Error_t errCode = NONE_ERROR;
 
 void triggerISR(void);
 
-#if defined(__IMXRT1062__)
-extern "C" uint32_t set_arm_clock(uint32_t frequency); //Serial.println(F_CPU_ACTUAL);
-extern float tempmonGetTemp(void); // in deg-celsius
-#endif
+// #if defined(__IMXRT1062__)
+// extern "C" uint32_t set_arm_clock(uint32_t frequency); //Serial.println(F_CPU_ACTUAL);
+// extern float tempmonGetTemp(void); // in deg-celsius
+// #endif
 
 void setup() {
   Serial.begin(115200);
  
   setup_port_io();
 
-  // Go into the waiting mode
+  // // Go into the waiting mode
   mode = WAITING;
 
-  // mode = ARMED;
-  // triggerISR();
+  mode = ARMED;
+  triggerISR();
 
   
 
@@ -34,40 +32,44 @@ void setup() {
 
 
 void loop() {
+  // Serial.println("HERE");
+  delay(100);
+  
 
-  if (mode == FIRING)
-  {
-    uint8_t currTriggerValue = digitalReadFast(TRIGGER_PIN);
 
-    if (currTriggerValue != lastTriggerValue)
-    {
-      DEBUG_PRINT("WARNING: Noise on trigger line after firing\n");
-      lastTriggerValue = currTriggerValue;
-    }
+  // if (mode == FIRING)
+  // {
+  //   uint8_t currTriggerValue = digitalReadFast(TRIGGER_PIN);
+
+  //   if (currTriggerValue != lastTriggerValue)
+  //   {
+  //     DEBUG_PRINT("WARNING: Noise on trigger line after firing\n");
+  //     lastTriggerValue = currTriggerValue;
+  //   }
     
-    captureTimeChange();
+  //   captureTimeChange();
 
-    if (ARM_DWT_CYCCNT > shotStopTime)
-    {
-      mode = WAITING;
+  //   if (ARM_DWT_CYCCNT > shotStopTime)
+  //   {
+  //     mode = WAITING;
 
-      DEBUG_PRINT("Number of triggers: %ld\n", gpioIdx);      
-      for(uint32_t i = 0 ; i < gpioIdx ; i++)
-      {
-        if (i > MAX_TRIGGERS_PER_SHOT)
-        {
-           DEBUG_PRINT("Too many beam triggers\n");
-           return;
-        }
+  //     DEBUG_PRINT("Number of triggers: %ld\n", gpioIdx);      
+  //     for(uint32_t i = 0 ; i < gpioIdx ; i++)
+  //     {
+  //       if (i > MAX_TRIGGERS_PER_SHOT)
+  //       {
+  //          DEBUG_PRINT("Too many beam triggers\n");
+  //          return;
+  //       }
         
-        volatile uint32_t timeval = gpioTimes[i];
-        volatile uint64_t output = unscrambleBits(gpio6Vals[i], gpio7Vals[i], gpio8Vals[i], gpio9Vals[i]);       
-        DEBUG_PRINT("\tTime: %3.9f Output: 0x%016llx\n", (double)timeval/(double)816000000, output);
-      }
-    }
-  }  
-  else
-  {
+  //       volatile uint32_t timeval = gpioTimes[i];
+  //       volatile uint64_t output = unscrambleBits(gpio6Vals[i], gpio7Vals[i], gpio8Vals[i], gpio9Vals[i]);       
+  //       DEBUG_PRINT("\tTime: %3.9f Output: 0x%016llx\n", (double)timeval/(double)816000000, output);
+  //     }
+  //   }
+  // }  
+  // else
+  // {
     for (int i = 0 ; i < 10000 ; i++)
     {
       long unsigned ts = ARM_DWT_CYCCNT;
@@ -92,7 +94,7 @@ void loop() {
     sumelapsed = 0;
     maxelapsed = 0;
     iter = 0;
-  }
+  // }
 }
 
 void triggerISR(void)
